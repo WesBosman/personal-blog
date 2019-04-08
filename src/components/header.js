84c52201ef1408903b420c89d7192f97538606d1
Wes Bosman <bosmanwn@vcu.edu>
2019-04-08 19:00:10 -0400

    working on the mobile and tablet menus

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,45 @@ import React from "react"
 import FontAwesome from "react-fontawesome"
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menuActive: false,
+    }
+
+    this.menuClicked = this.menuClicked.bind(this)
+  }
+  menuClicked() {
+    console.log("Menu has been clicked")
+    const active = this.state.menuActive
+
+    this.setState({
+      menuActive: !active,
+    })
+  }
   render() {
+    let active = this.state.menuActive
+    let activeClass = active ? "links active" : "links inactive"
+
     return (
       <nav className="header-nav">
         <div className="header-name-links">
           <div className="name">
             <a href="/">Wesley Bosman</a>
           </div>
-          <div className="links">
-            <a href="/">Home</a>
-            <a href="/gallery">Gallery</a>
-            <a href="/about">About</a>
-            <a href="https://github.com/WesBosman">
-              <FontAwesome name="github" size="2x" />
-            </a>
+          <div className="menu-links">
+            <div className={activeClass}>
+              <a href="/">Home</a>
+              <a href="/gallery">Gallery</a>
+              <a href="/about">About</a>
+              <a href="https://github.com/WesBosman">
+                <FontAwesome name="github" size="2x" />
+              </a>
+            </div>
+            <div className="menu" onClick={this.menuClicked}>
+              <FontAwesome name="bars" size="2x" />
+            </div>
           </div>
         </div>
       </nav>
